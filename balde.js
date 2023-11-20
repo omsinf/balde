28171fc2fd0b309b5244c3cb4a0bb4617f6ad998
Oliver Maximilian Schrader <omschrader@gmail.com>
2023-11-20 23:18:37 +0100

    refactor: focus in iiif

--- balde.js
+++ balde.js
@@ -1,3 +1,16 @@
+document.onload = initialLoad();
+
+function initialLoad() {
+    var checkboxes = document.getElementsByTagName("input");
+    for (c of checkboxes) {
+        c.checked = false;
+    }
+    var frames = document.getElementsByClassName("iiif");
+    for (f of frames) {
+        f.classList.add("display-none");
+    }
+}
+
 function selectText(textId) {
     var texts = document.getElementsByClassName("text");
     for (tx of texts) {
@@ -5,4 +18,9 @@ function selectText(textId) {
     }
     var textToShow = document.getElementById(textId);
     textToShow.style.display = "block";
+}
+
+function toggleEdition(editionId) {
+    const frame = document.getElementById(editionId);
+    frame.classList.toggle("display-none");
 }
