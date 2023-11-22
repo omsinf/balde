6218e549271a7387ff94331e55ad7fc4e5ffc6d1
Oliver Maximilian Schrader <omschrader@gmail.com>
2023-11-22 15:28:19 +0100

    refactor, add Müller 1844

--- script.js
+++ script.js
@@ -21,6 +21,33 @@ function selectText(textId) {
 }
 
 function toggleEdition(editionId) {
+    const optionField = document.getElementById(`hf_${editionId}`);
+    optionField.classList.toggle("option-selected");
     const frame = document.getElementById(editionId);
     frame.classList.toggle("display-none");
+}
+
+function goToText(editionId, textId) {
+    if (iiifRefs[editionId]) {
+        const refs = iiifRefs[editionId]
+        if (refs[textId]) {
+            const url = uvBaseUrl + refs["manifest"] + uvCanvas + refs[textId]
+            console.log(url)
+            document.getElementById(`iframe_${editionId}`).src = url;
+        } else {
+            console.error(`No textId found in editionId ${editionId} for textId ${textId}`)
+        }
+    } else {
+        console.error(`No refs found for editionId ${editionId}`)
+    }
+}
+
+const uvBaseUrl = "https://universalviewer.io/uv.html?manifest="
+const uvCanvas = "#&c=0&m=0&s=0&cv="
+
+const iiifRefs = {
+    "lyrica_1643": {
+        "manifest": "https://api.digitale-sammlungen.de/iiif/presentation/v2/bsb11270675/manifest",
+        "lyr_3_1": "135"
+    }
 }
