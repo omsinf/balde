46350aaba22dfec7b0fe3ba31c7cad8f93d14cab
Oliver Maximilian Schrader <omschrader@gmail.com>
2023-12-11 12:46:46 +0100

    refactor: use modules

---
+++ iiifRefs.mjs
@@ -0,0 +1,40 @@
+export const iiifRefs = {
+    "lyrica_1643": {
+        "manifest": "https://api.digitale-sammlungen.de/iiif/presentation/v2/bsb11270675/manifest",
+        "pages": {
+            "lyr_3_1": 135,
+            "lyr_3_31": 175,
+            "lyr_3_32": 176,
+            "lyr_3_33": 179,
+            "lyr_3_34": 180,
+            "lyr_3_35": 182,
+            "lyr_3_36": 184,
+        }
+    },
+    "lyrica_1660": {
+        "manifest": "https://api.digitale-sammlungen.de/iiif/presentation/v2/bsb10609929/manifest",
+        "pages": {
+            "lyr_3_1": 123,
+            "lyr_3_31": 162,
+            "lyr_3_32": 163,
+            "lyr_3_33": 166,
+            "lyr_3_34": 167,
+            "lyr_3_35": 169,
+            "lyr_3_36": 171,
+        }
+    },
+    "lyrica_1729": {
+        "manifest": "https://api.digitale-sammlungen.de/iiif/presentation/v2/bsb10712436/manifest",
+        "pages": {}
+    },
+    "lyrica_1805": {
+        "manifest": "https://api.digitale-sammlungen.de/iiif/presentation/v2/bsb10608714/manifest",
+        "pages": {}
+    },
+    "lyrica_1844": {
+        "manifest": "https://api.digitale-sammlungen.de/iiif/presentation/v2/bsb10608720/manifest",
+        "pages": {
+            "lyr_3_35": 287
+        }
+    }
+}
